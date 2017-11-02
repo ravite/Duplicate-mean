ee5ac16ce61f88434d4c3cbf16aef8fc23505c78
RAVITEJA <raviteja.g@tectoro.com>
2017-11-02 12:24:48 +0530

    remove password

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ require('rootpath')();
 var express = require('express');
 var app = express();
 var cors = require('cors');
- var bodyParser = require('body-parser');
- var expressJwt = require('express-jwt');
- var config = require('config.json');
- const mailer = require('express-mailer'); // call express
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+const mailer = require('express-mailer'); // call express
 app.set('views', __dirname + '/views');
 // set the view engine to pug
 app.set('view engine', 'pug');
@@ -19,13 +19,13 @@ mailer.extend(app, {
   transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
   auth: {
     user: 'ravitejagodavarthi@gmail.com', // gmail id
-    pass: '9705105735' // gmail password
+    pass: '********' // gmail password
   }
 });
 
 
 // test route to trigger emails
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   // Setup email data.
   var mailOptions = {
     to: 'ravitejagodavarthi@gmail.com',
@@ -37,7 +37,7 @@ app.get('/', function (req, res) {
   }
 
   // Send email.
-  app.mailer.send('email', mailOptions, function (err, message) {
+  app.mailer.send('email', mailOptions, function(err, message) {
     if (err) {
       console.log(err);
       res.send('There was an error sending the email');
@@ -48,28 +48,33 @@ app.get('/', function (req, res) {
 
 });
 
- app.use(cors());
- app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // use JWT auth to secure the api, the token can be passed in the authorization header or querystring
 app.use(expressJwt({
-    secret: config.secret,
-    getToken: function (req) {
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-            return req.headers.authorization.split(' ')[1];
-        } else if (req.query && req.query.token) {
-            return req.query.token;
-        }
-        return null;
+  secret: config.secret,
+  getToken: function(req) {
+    if (req.headers.authorization && req.headers.authorization.split(
+        ' ')[0] === 'Bearer') {
+      return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
     }
-}).unless({ path: ['/users/authenticate', '/users/register','/users'] }));
+    return null;
+  }
+}).unless({
+  path: ['/users/authenticate', '/users/register', '/users']
+}));
 
- // routes
+// routes
 app.use('/users', require('./controllers/users.controller'));
 
 // start server
 var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-var server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+var server = app.listen(port, function() {
+  console.log('Server listening on port ' + port);
 });
